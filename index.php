d867479cb2c962c00ab9216aa8bcd580f8a56e19
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-06-19 16:12:51 +0200

    add: function construct

--- index.php
+++ index.php
@@ -4,41 +4,46 @@ class movies
 {
     public $name;
     public $production;
-    public $type;
+    public $genres;
+    public $director;
 
-    public function __construct($name, $production , $type)
+    public function __construct($name, $production, $genres, $director)
     {
-        $this-> = name = $name
-        $this-> = production = $production;
+        $this->name = $name;
+        $this->production = $production;
+        $this->genres = $genres;
+        $this->director = $director;
     }
 }
 
-$harry = new movies();
-$harry->name = "Harry Potter";
-$harry->production = "2001";
+$harry = new movies("Harry Potter", "2001", "Adventure", "Alfonso Cuaron");
 
-
-echo 'nome film' . ' : ' . $harry->name;
+echo 'nome film' . ' : ' .  $harry->name;
 echo '<br>';
-echo 'produzione' . ' : ' .  $harry->production;
+echo 'produzione' . ' : '  . $harry->production;
+echo '<br>';
+echo 'Genere' . ' : '  . $harry->genres;
+echo '<br>';
+echo 'Regista' . ' : '  . $harry->director;
+echo '<br>';
+$fast = new movies("Fast & Furious", "2001", "Action", "Louis Leterrier");
+
+echo 'nome film' . ' : ' .  $fast->name;
+echo '<br>';
+echo 'produzione' . ' : '  . $fast->production;
+echo '<br>';
+echo 'Genere' . ' : '  . $fast->genres;
+echo '<br>';
+echo 'Regista' . ' : '  . $fast->director;
 echo '<br>';
 
-$fast = new movies();
-$fast->name = "Fast & Furious";
-$fast->production = "2001";
-
-
-echo 'nome film' . ' : ' . $fast->name;
-echo '<br>';
-echo 'produzione' . ' : ' . $fast->production;
-echo '<br>';
-
-$iron = new movies();
-$iron->name = "Iron Man";
-$iron->production =  "2008";
-
+$iron = new movies("Iron Man", "2008", "Action", "Jon Favreau");
 
 echo 'nome film' . ' : ' .  $iron->name;
 echo '<br>';
 echo 'produzione' . ' : '  . $iron->production;
+echo '<br>';
+echo 'Genere' . ' : '  . $iron->genres;
+echo '<br>';
+echo 'Regista' . ' : '  . $iron->director;
 echo '<br>';
